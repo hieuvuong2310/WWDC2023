8b5c118efbdbb75dbe8c8214fd2a9570696fe4b2
Trong Hieu Vuong <hieuvg02@gmail.com>
2023-04-17 23:06:09 -0700

    Finalize dish view.

---
+++ What to eat.swiftpm/PersonalIntro/PersonalIntroView.swift
@@ -0,0 +1,20 @@
+//
+//  SwiftUIView.swift
+//  
+//
+//  Created by Hieu Vuong on 2023-04-17.
+//
+
+import SwiftUI
+
+struct PersonalIntroView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct PersonalIntroView_Previews: PreviewProvider {
+    static var previews: some View {
+        PersonalIntroView()
+    }
+}
